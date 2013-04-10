849db5f241489a962a04452ee588f59901a3ef85
BRuNTza <drbking@gmail.com>
2013-04-10 21:00:56 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: brunt
+ *
+ * Created on 08 April 2013, 10:01 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    
+    
+    return 0;
+}
+
