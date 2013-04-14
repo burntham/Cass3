ffb2e422a939e05fba858ea446578713c19b80ca
Daniel <drbking@gmail.com>
2013-04-14 17:33:00 +0200

    Coming along...

--- container.h
+++ container.h
@@ -8,10 +8,25 @@
 #ifndef CONTAINER_H
 #define	CONTAINER_H
 #include "widget.h"
+#include <stack>
+#include "counter.h"
+
 namespace brndan022
-{
-    class container : widget
-    {
+
+{       
+    //abstract class
+    class container :public widget
+    {   
+    protected:
+        //store a list of pointers to be killed later
+         std::stack<widget*> children;    
+   
+    public:
+        
+        virtual void add_child(widget * newChild)= 0;
+        virtual ~container(){ 
+            
+        };
         
     };
 }
